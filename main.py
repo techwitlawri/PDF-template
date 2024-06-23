d149f72d5ab59e0a946ab82c0870577a51464e64
Mmachi Ezeh <mmachilawri@gmail.com>
2024-06-23 21:23:46 +0100

    pdf template

---
+++ main.py
@@ -0,0 +1,18 @@
+from fpdf import FPDF
+import pandas as pd
+
+
+pdf= FPDF(orientation= "P", unit = "mm", format= "a4")
+
+df= pd.read_csv("topics (1).csv")
+
+for index, row in df.iterrows():
+    pdf.add_page()
+
+    pdf.set_font(family= "Times", style= "B", size= 24)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w= 0, h=12, txt= row["Topic"], align="l", ln=1)
+    pdf.line(x1=10,y1= 21, x2=200, y2=21)
+
+
+pdf.output("output.pdf")
